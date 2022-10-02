e8a57b291a282466c8a3f98d0c918f43929fcd25
Saurabh <singhsaurabh162000@gmail.com>
2022-10-02 16:28:25 +0530

    adding .env file

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const database = require('./configs/db');
-
+const authRouter = require('./routes/auth');
 const app = express();
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    return res.send('Hello World!');
 });
 
+app.use("/api/auth", authRouter);
 
 app.listen(8080, () => {
     database();
