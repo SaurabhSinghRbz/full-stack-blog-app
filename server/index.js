23e620c40afaa64b1bb505121f2fec5bd2d72795
Saurabh <singhsaurabh162000@gmail.com>
2022-10-02 21:09:02 +0530

    uploading file done

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const authRouter = require('./routes/auth');
 const categoryRouter = require('./routes/categoery.routes');
 const postRouter = require('./routes/post.routes');
 const userRouter = require('./routes/user.routes');
+const multer = require('multer');
+
+
 const app = express();
 app.use(express.json());
 
@@ -12,10 +15,29 @@ app.get('/', (req, res) => {
     return res.send('Hello World!');
 });
 
+
+const myStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'Images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: myStorage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    return res.status(200).json('File has been uploaded');
+});
+
+
+
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/categories", categoryRouter);
+
+
 
 app.listen(8080, () => {
     database();
