7c7aad85e22c4f21a1e8853d6b9b7ca09bc3be04
Saurabh <singhsaurabh162000@gmail.com>
2022-10-02 18:06:22 +0530

    deleting the user's account done

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,19 @@
 const userRouter = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const Post = require('../models/Post');
+
+
+
+
+
+
+
+
 
 // update the user or update the user's password
 userRouter.put('/:id', async (req, res) => {
-    if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.userId === req.params.id) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -28,6 +37,25 @@ userRouter.put('/:id', async (req, res) => {
 
 
 
+// delete the user or delete the user's account
+userRouter.delete('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            try {
+                await Post.deleteMany({ username: user.username });
+                const user = await User.findByIdAndDelete(req.params.id);
+                return res.status(200).send('User has been deleted');
+            } catch (err) {
+                return res.status(500).send(err);
+            }
+        } catch (error) {
+            return res.status(500).send("User not found");
+        }
 
+    } else {
+        return res.status(403).send('You can only delete your account');
+    }
+});
 
 module.exports = userRouter;
