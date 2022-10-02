fd9f3e2ca294e334581cfb1ef9fb033acf92e6c7
Saurabh <singhsaurabh162000@gmail.com>
2022-10-02 17:40:32 +0530

    login+register completed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,13 +13,33 @@ Router.post('/register', async (req, res) => {
         email: req.body.email,
         password: hashedPassword,
     });
-
     // alternate way to create a new user
     // const newUser = new User(req.body);
-
     try {
         const user = await newUser.save();
         return res.status(200).send(user);
+    } catch (err) {
+        return res.status(500).send(err);
+    }
+});
+
+
+
+// LOGIN
+Router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(400).send('Invalid password');
+        }
+
+        const { password, ...otherProp } = user._doc;
+        return res.status(200).send(otherProp);
     } catch (err) {
         return res.status(500).send(err);
     }
