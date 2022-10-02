96de5fcaf2ccc04054bf3509e30c06e5a3deeb3e
Saurabh <singhsaurabh162000@gmail.com>
2022-10-02 16:10:40 +0530

    schema created

---
+++ server/models/Category.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema({
+    name: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Category', CategorySchema);
